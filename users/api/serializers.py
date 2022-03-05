30b07f868cc2854f9669e0cbb9b105361b3023b0
Mohamed Awad <awadmohamed233@gmail.com>
2022-03-06 01:23:57 +0200

    Fix:- fix save password as plaine text

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -12,3 +12,12 @@ class UserSerializer(serializers.ModelSerializer):
             'password',
         ]
         read_only_fields = ['id']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
